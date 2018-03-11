c04890c1d9ba47cf2e7f512932afab00a4ecb076
CarolinePaixao <CarolinePaixao@users.noreply.github.com>
2018-03-11 01:18:25 -0300

    Atualizando conexão com banco de dados 8

--- connect.php
+++ connect.php
@@ -6,11 +6,10 @@
  * Time: 16:21
  */
 try {
-    
     $database_host     = '172.30.229.248';
-    $database_username = getenv('MYSQL_USER');
-    $database_password = getenv('MYSQL_ROOT_PASSWORD');
-    $database_name     = getenv('MYSQL_DATABASE');
+    $database_username = 'userT3R';
+    $database_password = '0ijXrhfwFmdjU3B6';
+    $database_name     = 'sampledb';
     $conn = new PDO('mysql:host='.$database_host.';dbname='.$database_name.', '.$database_username.', '.$database_password); //Estabelecendo uma conexão com o bd
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Estabelecendo atributos para caso de algum erro
 } catch(PDOException $e) {
