49134a345f3d1cb2fdacf777ed763a3356c2a63b
Caroline Paixão <cbp_caroline@hotmail.com>
2018-03-11 13:09:33 -0300

    Teste de conexão

--- index.php
+++ index.php
@@ -5,8 +5,28 @@
  * Date: 10/03/2018
  * Time: 13:17
  */
-include_once ('connect.php');
-?>
+echo 'Inicio página';
+$host = getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' .getenv('OPENSHIFT_MYSQL_DB_PORT');
+$user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$db = getenv('OPENSHIFT_GEAR_NAME');
+echo ' após váriaveis';
+//$host = '172.30.229.248:3306';
+//$db   = "sampledb";
+//$user = "userT3R";
+//$pass = "0ijXrhfwFmdjU3B6";
+$dns = 'mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8';
+echo $dns;
+echo ' após dns';
+try {
+
+
+    $conn = new PDO($dns, $user, $pass); //Estabelecendo uma conexão com o bd
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Estabelecendo atributos para caso de algum erro
+} catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage(); //Mostrando erro.
+}
+echo ' após tentavia de conexão';?>
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
