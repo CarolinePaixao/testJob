583e6a77e08e52d4514c8be8cee9800ab093ff9b
Caroline Paixão <cbp_caroline@hotmail.com>
2018-03-11 13:27:59 -0300

    Teste de conexão 3

--- index.php
+++ index.php
@@ -6,16 +6,11 @@ Teste, inicio de página
  * Date: 10/03/2018
  * Time: 13:17
  */
-echo 'Inicio página';
-$host = getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' .getenv('OPENSHIFT_MYSQL_DB_PORT');
-$user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-$db = getenv('OPENSHIFT_GEAR_NAME');
-echo ' após váriaveis';
-//$host = '172.30.229.248:3306';
-//$db   = "sampledb";
-//$user = "userT3R";
-//$pass = "0ijXrhfwFmdjU3B6";
+
+$host = '172.30.229.248:3306';
+$db   = "sampledb";
+$user = "userT3R";
+$pass = "0ijXrhfwFmdjU3B6";
 $dns = 'mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8';
 echo $dns;
 echo ' após dns';
